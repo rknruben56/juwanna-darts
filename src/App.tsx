5b5276aad4c47a53a911600ba08fec085c5bc9f6
Ruben Rodriguez <rrodriguez6@outlook.com>
2021-12-07 17:02:47 -0600

    Remove boilerplate from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-// import './App.css';
 import { Navbar, Container } from 'react-bootstrap';
 import styled from '@emotion/styled';
 
@@ -21,22 +19,6 @@ function App() {
         </Container>
       </Navbar>
     </>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
